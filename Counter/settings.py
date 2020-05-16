9466e426de45ce541d61dca1c45bede30646fec7
jonem3 <r0sshutte>
2020-05-16 19:35:21 +0100

    Fixed stuff, added caching

--- Counter/settings.py
+++ Counter/settings.py
@@ -119,7 +119,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'apache_static')
 
 CACHES = {
     'default': {
